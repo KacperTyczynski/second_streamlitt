f414e356d8de40da49759870aa46f6a51d69b365
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2023-05-26 13:48:41 +0200

    Update main.py
    Fixing Select statement in cursor execution line

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 # Creating a catalog with data from snowflake
+my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 df = pandas.DataFrame(my_catalog)
