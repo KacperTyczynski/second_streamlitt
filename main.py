b91547afab6331435a73dfc7ca18354749490b58
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2023-05-26 13:33:25 +0200

    Update main.py
    Fixed a typo

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_sweatsuit_data(this_cloth_choice):
   cloth_normalized = pandas.json_normalize(cloth_response.json())
   return cloth_normalized
 
-streamlit.header('ZENA'S AMAZING ATHLEISURE CATALOG')
+streamlit.header('ZENA"'"S AMAZING ATHLEISURE CATALOG')
 try:
   cloth_choice = streamlit.text_input('Pick a sweatsuit color or style:')
   if not cloth_choice:
