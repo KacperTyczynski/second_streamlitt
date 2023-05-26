6caa7930d0f26db399f8a8724d667d026f44b4ce
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2023-05-26 13:53:11 +0200

    Update main.py
    Finished app

--- main.py
+++ main.py
@@ -4,24 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-def get_sweatsuit_data(this_cloth_choice):
-  cloth_response = requests.get("https://uni-klaus.s3.us-west-2.amazonaws.com/clothing" +this_cloth_choice)
-  cloth_normalized = pandas.json_normalize(cloth_response.json())
-  return cloth_normalized
-
-streamlit.header('ZENA S AMAZING ATHLEISURE CATALOG')
-try:
-  cloth_choice = streamlit.text_input('Pick a sweatsuit color or style:')
-  if not cloth_choice:
-    streamlit.error("Please select a sweatsuit")
-  else:  
-    back_from_function = get_sweatsuit_data(cloth_choice)
-    streamlit.dataframe(back_from_function)
-
-except URLError as e:
-  streamlit.error()
-
-  
+ 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
@@ -30,14 +13,30 @@ my_cur.execute("select color_or_style from catalog_for_website")
 my_catalog = my_cur.fetchall()
 
 df = pandas.DataFrame(my_catalog)
-streamlit.write(df)
+# streamlit.write(df)
 
 color_list = df[0].values.tolist()
-print(color_list)
+# print(color_list)
 
+# Adding a color/style selection box
+option = streamlit.selectbox('Pick a sweatsuit color or style:', list(color_list))
+
+# Adding a description
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
 
 ## CONNECTION TEST
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
 #my_data_row = my_cur.fetchone()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
+
+my_cur.execute("select direct_url, price, size_list, upsell_product_desc from catalog_for_website where color_or_style = '" + option + "';")
+df2 = my_cur.fetchone()
+streamlit.image(
+df2[0],
+width=400,
+caption = product_caption
+)
+streamlit.write('Price: ', df2[1])
+streamlit.write('Sizes Available: ',df2[2])
+streamlit.write(df2[3])
